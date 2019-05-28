d6a8e3f6a2296361da285d9d2793188cd5806e49
Anh Tran <anh.tran@9dots.org>
2019-05-28 16:14:14 -0700

    add commented out outline of data structure in fb

---
+++ src/firebase/collectionFunctions.js
@@ -0,0 +1,18 @@
+import { usersCollectionRef } from "./firebase";
+
+/*
+db = firebase.firestore();
+usersRef = db.collection("users");
+
+user = usersRef.doc(uid);
+
+user field => userInformation
+
+itemCollections = usersRef.doc(uid).collection("itemCollections").doc(collectionUID)
+
+itemCollections field => color, collaborators...
+
+item = itemCollections.collection("item").doc(itemID)
+
+item field => text, isComplete...
+*/
