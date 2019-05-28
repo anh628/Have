60e90163ed0ba0e02211819c86abb2135783de21
Anh Tran <anh.tran@9dots.org>
2019-05-28 16:16:38 -0700

    edited comments

--- src/firebase/collectionFunctions.js
+++ src/firebase/collectionFunctions.js
@@ -2,17 +2,17 @@ import { usersCollectionRef } from "./firebase";
 
 /*
 db = firebase.firestore();
-usersRef = db.collection("users");
+usersCollectionRef = db.collection("users");
 
-user = usersRef.doc(uid);
+userRef = usersRef.doc(uid);
 
 user field => userInformation
 
-itemCollections = usersRef.doc(uid).collection("itemCollections").doc(collectionUID)
+itemCollectionsRef = usersRef.doc(uid).collection("itemCollections").doc(collectionUID)
 
 itemCollections field => color, collaborators...
 
-item = itemCollections.collection("item").doc(itemID)
+itemRef = itemCollections.collection("item").doc(itemID)
 
 item field => text, isComplete...
 */
