7196a67b50094f63703eea00e670c238040aae4f
alexis.cabrera@9dots.org <alexis.cabrera@9dots.org>
2019-05-31 09:43:23 -0700

    firebase!

--- src/firebase/collectionFunctions.js
+++ src/firebase/collectionFunctions.js
@@ -1,4 +1,4 @@
-import { usersCollectionRef } from "./firebase";
+import { usersCollectionRef, db } from "./firebase";
 
 /*
 db = firebase.firestore();
@@ -17,6 +17,7 @@ itemRef = itemCollections.collection("item").doc(itemID)
 item field => text, isComplete...
 */
 
+
 export const addCollection = (uid, collectionId, collectionColor) => {
   const collectionInfo = {
     title: null,
@@ -25,9 +26,9 @@ export const addCollection = (uid, collectionId, collectionColor) => {
   };
   return usersCollectionRef
     .doc(`${uid}`)
-    .collection("itemCollections")
-    .doc(`${collectionId}`)
-    .set(collectionInfo)
+    .collection("itemCollections") //collection of items for a given user
+    .doc(`${collectionId}`) //this will pick amongst collections that an individual user will have
+    .set(collectionInfo) //will be the fields above
     .catch(error => console.log(error));
 };
 
@@ -60,15 +61,15 @@ export const addItem = (uid, collectionId, itemId, text) => {
   const itemInfo = {
     itemId,
     text,
-    isComplete: false
+    isComplete: false  
   };
   return usersCollectionRef
     .doc(`${uid}`)
     .collection("itemCollections")
-    .doc(`${collectionId}`)
-    .collection("item")
-    .doc(`${itemId}`)
-    .set(itemInfo)
+    .doc(`${collectionId}`) 
+    .collection("items") //the items within a user's collection
+    .doc(`${itemId}`) //the ID of the item you want to add
+    .set(itemInfo) //see above
     .catch(error => console.log(error));
 };
 
@@ -77,7 +78,7 @@ export const editItem = (uid, collectionId, itemId, editedText) => {
     .doc(`${uid}`)
     .collection("itemCollections")
     .doc(`${collectionId}`)
-    .collection("item")
+    .collection("items")
     .doc(`${itemId}`);
 
   return itemRef
@@ -86,15 +87,36 @@ export const editItem = (uid, collectionId, itemId, editedText) => {
     .catch(error => console.log(error));
 };
 
+//IF we want trash/archive we will need to edit this 
 export const deleteItem = (uid, collectionId, itemId) => {
   return usersCollectionRef
     .doc(`${uid}`)
     .collection("itemCollections")
     .doc(`${collectionId}`)
-    .collection("item")
+    .collection("items")
     .doc(`${itemId}`)
     .delete()
     .catch(error => console.log(error));
 };
 
-export const deleteAllCompleted = (uid, collectionId) => {};
+export const deleteAllCompleted = (uid, collectionId) => {
+    const batch = db.batch();
+
+    const currentCollectionRef = usersCollectionRef
+    .doc(`${uid}`)
+    .collection("itemCollections")
+    .doc(`${collectionId}`)
+    .collection("items");
+
+    currentCollectionRef.where("isComplete", "==", true)
+    .get()
+    .then(completedItems => {
+        completedItems.forEach(item => {
+            const itemRef = currentCollectionRef.doc(item.itemId);
+            batch.delete(itemRef)
+        
+    } return batch.commit()
+})
+     
+
+};
