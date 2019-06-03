cb6cc2c64c15968996201505929fc43e6a700c56
Anh Tran <anh.tran@9dots.org>
2019-06-03 14:19:03 -0700

    comments and empty route page

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { store } from './store'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import { store } from "./store";
+import Route from "./components/Route";
 
-
-store.subscribe(
-    throttle(() => {
-    saveState({
-      collection: store.getState().collection});
-  
-  }, 1000))
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-{/* <Root store={store}> will need this eventually 
-
-    <App />  
-  </Root>, document.getElementById('root')); */}
-
+ReactDOM.render(<Route store={store} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import { reactReduxFirebase } from "react-redux-firebase";
 import { reduxFirestore } from "redux-firestore";
-import { firebase } from "./firebase/fb";
-import rootReducer from "./reducers/rootreducer";
+import { firebase } from "./firebase/firebase";
+import rootReducer from "./reducers/rootReducer";
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+//import thunk from "redux-thunk";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const createStoreWithFirebase = compose(
   reactReduxFirebase(firebase, {}), // firebase instance as first argument, {} is for the react-redux-firebase config
@@ -15,6 +15,6 @@ const createStoreWithFirebase = compose(
 // Add reactReduxFirebase enhancer when making store creator
 export const store = createStoreWithFirebase(
   rootReducer,
-  {}, // persistedState,
-  composeEnhancers(applyMiddleware(thunk))
+  {} //  <- initial state , persistedState
+  //composeEnhancers(applyMiddleware(thunk)) // <- for redux dev tools
 );
