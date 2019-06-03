f6a6fdf0b0441477608ef5cc8d4585bbf5fa674a
Anh Tran <anh.tran@9dots.org>
2019-06-03 09:03:45 -0700

    added comments and thunk

--- src/store.js
+++ src/store.js
@@ -1,21 +1,20 @@
-import { reactReduxFirebase } from 'react-redux-firebase';
-import { reduxFirestore } from 'redux-firestore';
-import { firebase } from './firebase/fb'
-import rootReducer from './reducers/rootreducer'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { reactReduxFirebase } from "react-redux-firebase";
+import { reduxFirestore } from "redux-firestore";
+import { firebase } from "./firebase/fb";
+import rootReducer from "./reducers/rootreducer";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const createStoreWithFirebase = compose(
-  reactReduxFirebase(firebase, {}), // firebase instance as first argument
+  reactReduxFirebase(firebase, {}), // firebase instance as first argument, {} is for the react-redux-firebase config
   reduxFirestore(firebase) // <- needed if using firestore
-)(createStore)
+)(createStore);
 
-
+// Add reactReduxFirebase enhancer when making store creator
 export const store = createStoreWithFirebase(
-    rootReducer,
-    {}, // persistedState, 
-    composeEnhancers(applyMiddleware(thunk))
-)
+  rootReducer,
+  {}, // persistedState,
+  composeEnhancers(applyMiddleware(thunk))
+);
