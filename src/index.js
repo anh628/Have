0da3faf23cd05fb1bc74755df79403c9fa92b47d
alexis.cabrera@9dots.org <alexis.cabrera@9dots.org>
2019-06-03 15:01:26 -0700

    working on getting the store working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import { store } from "./store";
-import Route from "./components/Route";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
+import { store } from './store'
+// import PropPrint from './components/DummyPropsPrinter/PropPrint'
+import App from './App'
 
-ReactDOM.render(<Route store={store} />, document.getElementById("root"));
+ReactDOM.render(<App store={store} />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
