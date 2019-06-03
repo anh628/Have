340878a3ab82f9d8f54d8809c80105fc816b5c45
Anh Tran <anh.tran@9dots.org>
2019-06-03 15:50:09 -0700

    create action and reducer for editItemFlag

--- src/reducers/editItemFlag.js
+++ src/reducers/editItemFlag.js
@@ -1,13 +1,13 @@
 import * as actionType from '../constants/constants'
 
-const  editItemFlag = (state = [], action)=> {
-    switch(action.type){
-        case actionType.EDIT_ITEM_FLAG:
-            state.map(item => 
-                item.id === action.id ? 
-                    {...items, editing : !item.editing }:
-                     item)
-        }
-    }
-
+const editItemFlag = (state = [], action) => {
+  switch (action.type) {
+    case actionType.EDIT_ITEM_FLAG:
+      let item = { ...action.item }
+      item = { ...item, editing: !action.item.editing }
+      return item
+    default:
+      return state
+  }
+}
 export default editItemFlag
