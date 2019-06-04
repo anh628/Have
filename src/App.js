54029552d67b85b34158647cb0412de7dd7cbe4e
alexis.cabrera@9dots.org <alexis.cabrera@9dots.org>
2019-06-03 17:16:19 -0700

    working on the add bar trying to figure out how to map state to props so that I can access a colletion ID if I need to

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import PropsPrinter from './components/DummyPropsPrinter/PropsPrinter'
+// import PropsPrinter from './components/DummyPropsPrinter/PropsPrinter'
+import AddBar from './components/AddBar'
 
 function App () {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          <PropsPrinter />
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
+        <AddBar />
       </header>
     </div>
   )
