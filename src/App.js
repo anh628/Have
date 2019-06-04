fd79f0076b45f9094439adfa3cf7ea8174965cd5
Anh Tran <anh.tran@9dots.org>
2019-06-04 09:39:25 -0700

    added login to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import PropsPrinter from './components/DummyPropsPrinter/PropsPrinter'
+import { firestoreConnect, withFirebase } from 'react-redux-firebase'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import AuthenticationButton from './components/AuthenticationButton'
 
 function App () {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          <PropsPrinter />
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <AuthenticationButton />
     </div>
   )
 }
 
-export default App
+export default compose(
+  firestoreConnect(props => [
+    {
+      collection: 'users',
+      doc: props.uid
+      // subcollections: [
+      //   {
+      //     collection: 'itemCollections',
+      //     doc: , // props.collectionId
+      //     subcollections: [
+      //       {
+      //         collection: 'items'
+      //       }
+      //     ]
+      //   }
+      // ]
+    }
+  ]),
+  withFirebase,
+  connect(state => {
+    return {
+      uid: state.firebase.auth.uid // uid passed as props.uid
+    }
+  })
+)(App)
