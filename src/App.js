2dfce79e4feb88452f42369162cc554790d0c12e
Julius Diaz Panoriñgan <julius.dp@9dots.org>
2019-06-06 16:15:58 -0700

    ensure all components in App

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from 'react'
 import './App.css'
 import { connect } from 'react-redux'
 import AuthenticationButton from './components/AuthenticationButton'
+import AddBar from './components/addBar'
 import List from './components/List'
 
 const App = ({ uid }) => {
   return (
     <div className='App'>
       <AuthenticationButton />
+      <header className='App-header'>
+        <AddBar />
+      </header>
       <List uid={uid} />
     </div>
   )
 }
 
 export default connect(state => ({
-  uid: state.firebase.auth.uid
+  uid: state.firebase.auth.uid // uid passed as props.uid
 }))(App)
