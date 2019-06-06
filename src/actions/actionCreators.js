ab389d9e5e6f558d710afabc637e261a61762d6f
Anh Tran <anh.tran@9dots.org>
2019-06-06 11:20:42 -0700

    edits to collectionFunctions and actionCreators

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,17 +1,17 @@
-import * as actionType from "../constants/constants";
+import * as actionType from '../constants/constants'
 
 export const addCollection = (uid, collectionId, collectionColor) => ({
   type: actionType.ADD_COLLECTION,
   uid,
   collectionId,
   collectionColor
-});
+})
 
 export const deleteCollection = (uid, collectionId) => ({
   type: actionType.DELETE_COLLECTION,
   uid,
   collectionId
-});
+})
 
 // export const addTitle = (uid, collectionId, title) => ({
 //     type: actionType.ADD_TITLE,
@@ -25,13 +25,13 @@ export const editTitle = (uid, collectionId, title) => ({
   uid,
   collectionId,
   title
-});
+})
 
 export const deleteTitle = (uid, collectionId) => ({
   type: actionType.DELETE_TITLE,
   uid,
   collectionId
-});
+})
 
 export const addItem = (uid, collectionId, itemId, text) => ({
   type: actionType.ADD_ITEM,
@@ -39,7 +39,7 @@ export const addItem = (uid, collectionId, itemId, text) => ({
   collectionId,
   itemId,
   text
-});
+})
 
 export const editItem = (uid, collectionId, itemId, editedText) => ({
   type: actionType.EDIT_ITEM,
@@ -47,20 +47,20 @@ export const editItem = (uid, collectionId, itemId, editedText) => ({
   collectionId,
   itemId,
   editedText
-});
+})
 
 export const deleteItem = (uid, collectionId, itemId) => ({
   type: actionType.DELETE_ITEM,
   uid,
   collectionId,
   itemId
-});
+})
 
 export const deleteAllCompleted = (uid, collectionId) => ({
   type: actionType.DELETE_ALL_COMPLETED,
   uid,
   collectionId
-});
+})
 
 // export const addImage = (uid, collectionId, image) => ({
 //   type: actionType.ADD_IMAGE,
@@ -74,63 +74,61 @@ export const editImage = (uid, collectionId, editedImage) => ({
   uid,
   collectionId,
   editedImage
-});
+})
 
-export const deleteImage = (uid, collectionId, image) => ({
+export const deleteImage = (uid, collectionId) => ({
   type: actionType.DELETE_IMAGE,
   uid,
-  collectionId,
-  image
-});
+  collectionId
+})
 
 export const toggleItem = (uid, collectionId, itemId) => ({
   type: actionType.TOGGLE_ITEM,
   uid,
   collectionId,
   itemId
-});
+})
 
 export const toggleAllItems = (uid, collectionId) => ({
   type: actionType.TOGGLE_ALL_ITEMS,
   uid,
   collectionId
-});
+})
 
 export const addColor = (uid, collectionId, collectionColor) => ({
   type: actionType.ADD_COLOR,
   uid,
   collectionId,
   collectionColor
-});
+})
 
 export const editColor = (uid, collectionId, collectionColor) => ({
   type: actionType.EDIT_COLOR,
   uid,
   collectionId,
   collectionColor
-});
-
+})
 
 export const addCollaborator = (uid, collectionId, collabUID) => ({
   type: actionType.ADD_COLLABORATOR,
   uid,
   collectionId,
   collabUID
-});
+})
 
 export const removeCollaborator = (uid, collectionId, collabUID) => ({
   type: actionType.REMOVE_COLLABORATOR,
   uid,
   collectionId,
   collabUID
-});
+})
 
 export const logIn = uid => ({
   type: actionType.LOG_IN,
   uid
-});
+})
 
 export const logOut = uid => ({
   type: actionType.LOG_,
   uid
-});
+})
