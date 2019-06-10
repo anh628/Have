7c49d2027f98917aae5b6a84b12cd157b0b7e0cb
alexis.cabrera@9dots.org <alexis.cabrera@9dots.org>
2019-06-10 15:17:09 -0700

    modal 2 started, but won't close properly

--- src/App.js
+++ src/App.js
@@ -5,53 +5,15 @@ import NewCollection from './components/NewCollection'
 import React, { Component } from 'react'
 import List from './components/List'
 
-// const App = ({ uid }) => {
-//   return (
-//     <div className='App'>
-//       <AuthenticationButton />
-//       <header className='App-header'>
-//         <NewCollection uid={uid} />
-//       </header>
-//       {/* <List uid={uid} /> */}
-//     </div>
-//   )
-// }
-
 class App extends Component {
-  // constructor (props) {
-  //   super(props)
-  //   this.state = {
-  //     visible: false
-  //   }
-  // }
-
-  // openModal () {
-  //   this.setState({
-  //     visible: true
-  //   })
-  // }
-
-  // closeModal () {
-  //   this.setState({
-  //     visible: false
-  //   })
-  // }
-
   render () {
     return (
       <div className='App'>
         <AuthenticationButton />
         <header className='App-header'>
-          {/* <Modal
-            visible={this.state.visible}
-            width='400'
-            height='300'
-            effect='fadeInUp'
-            onClickAway={() => this.closeModal()}> */}
           <NewCollection uid={this.props.uid} />
-          {/* </Modal> */}
         </header>
-        {/* <List uid={this.props.uid} /> */}
+        <List uid={this.props.uid} />
       </div>
     )
   }
--- src/components/ItemCollection.js
+++ src/components/ItemCollection.js
@@ -6,61 +6,92 @@ import { firestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
 import { changeEditCollectionFlag } from '../actions/actionCreators'
 import CollectionView from './CollectionView'
 import Footer from './Footer'
+import Modal from 'react-responsive-modal'
 
-const ItemCollection = ({
-  uid,
-  collectionId,
-  collectionColor,
-  title,
-  items,
-  editCollectionFlag,
-  changeEditCollectionFlag,
-  image
-}) => {
-  if (editCollectionFlag) {
-    return <CollectionView collectionId={collectionId} />
-  } else {
-    const keys = items ? Object.keys(items) : null
+class ItemCollection extends React.Component {
+  state = {
+    open: false
+  }
 
-    const itemsList = !isLoaded(items)
+  openModal () {
+    this.setState({
+      open: true
+    })
+  }
+
+  closeModal () {
+    this.setState({
+      open: false
+    })
+  }
+
+  render () {
+    const keys = this.props.items ? Object.keys(this.props.items) : null
+
+    const itemsList = !isLoaded(this.props.items)
       ? 'loading'
-      : isEmpty(items)
+      : isEmpty(this.props.items)
         ? 'Item Collection list is empty'
         : keys
           ? keys.map(itemId => (
             <Item
               key={itemId}
-              uid={uid}
-              collectionId={collectionId}
-              itemId={itemId}
-              {...items[itemId]} />
+              uid={this.props.uid}
+              collectionId={this.props.collectionId}
+              itemId={this.props.itemId}
+              {...this.props.items[itemId]} />
           ))
           : null
 
     const uncheckedItems = keys
-      ? keys.filter(itemId => items[itemId].isComplete === false).length > 0
+      ? keys.filter(itemId => this.props.items[itemId].isComplete === false)
+        .length > 0
       : null
     const checkItems = keys
-      ? keys.filter(itemId => items[itemId].isComplete === true).length > 0
+      ? keys.filter(itemId => this.props.items[itemId].isComplete === true)
+        .length > 0
       : null
-    const displayImage = image ? <img src={image} alt='cover-art' /> : null
+    const displayImage = this.props.image ? (
+      <img src={this.props.image} alt='cover-art' />
+    ) : null
+
+    const modal = (
+      <Modal
+        open={this.state.open}
+        styles={{
+          modal: {
+            backgroundColor: this.props.collectionColor
+          }
+        }}
+        width='400'
+        height='300'
+        effect='fadeInUp'
+        onClose={() => this.closeModal()}>
+        <CollectionView
+          uid={this.props.uid}
+          collectionId={this.props.collectionId}
+          title={this.props.title} />
+      </Modal>
+    )
 
     return (
       <div
-        style={{ backgroundColor: collectionColor }}
-        className='item-collection'>
+        style={{ backgroundColor: this.props.collectionColor }}
+        className='item-collection'
+        onClick={() => this.openModal()}>
         {/* <div onClick={() => changeEditCollectionFlag(collectionId)}> */}
         <div>
           {displayImage}
-          <h2 className='item-collection-title'>{title}</h2>
+          <h2 className='item-collection-title'>{this.props.title}</h2>
           <div>{itemsList}</div>
         </div>
         <Footer
-          uid={uid}
-          collectionId={collectionId}
-          areItems={!!items}
+          uid={this.props.uid}
+          collectionId={this.props.collectionId}
+          areItems={!!this.props.items}
           uncheckedItems={uncheckedItems}
           checkItems={checkItems} />
+        {modal}
       </div>
     )
   }
