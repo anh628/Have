0af900ea77bf278413865b6096fc4847f90f4126
Anh Tran <anh.tran@9dots.org>
2019-06-25 16:14:30 -0700

    clicking done will automatically add anything writen in the add new item bar

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -3,26 +3,44 @@ import { addItem } from '../firebase/collectionFunctions'
 
 // props passed in: uid, collectionId
 class NewItem extends React.Component {
+  state = {
+    text: null
+  }
+
+  /*
+  function for when you leave the add new item bar
+  */
+  handleBlur = () => {
+    if (this.state.text && this.state.text.trim()) {
+      addItem(this.props.uid, this.props.collectionId, this.state.text.trim())
+    }
+  }
+
+  handleChange = event => {
+    this.setState({ text: event.currentTarget.value })
+  }
+
+  /*
+  function that listen to the keys being typed
+  determines when to go to handleBlur
+  */
+  handleKeyDown = event => {
+    if (event.key === 'Enter') {
+      this.handleBlur()
+    }
+  }
   render () {
-    let input
     return (
       <div>
-        <form
-          onSubmit={e => {
-            e.preventDefault()
-            if (!input.value.trim()) {
-              input.value = ' '
-            }
-            addItem(this.props.uid, this.props.collectionId, input.value)
-            input.value = ''
-          }}>
-          <input
-            className='addItem'
-            type='text'
-            ref={node => (input = node)}
-            placeholder='add new item'
-            autoFocus={true} />
-        </form>
+        <input
+          className='addItem'
+          type='text'
+          placeholder='add new item'
+          autoFocus={true}
+          value={this.state.text}
+          onBlur={this.handleBlur}
+          onChange={this.handleChange}
+          onKeyDown={this.handleKeyDown} />
       </div>
     )
   }
